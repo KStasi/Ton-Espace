6762f7298149a3bec5a906c1e85a61ac8e058b33
kstasi <nastasiakondaurova@gmail.com>
2019-12-09 22:18:24 +0200

    update code

--- scripts/04_broadcast_new_planet.sh
+++ scripts/04_broadcast_new_planet.sh
@@ -1,5 +1,5 @@
 #!/bin/sh
-fift -s scripts/wallet.fif "build/main-wallet" "0f_mdBk0Tm0T_zUS4u-WR2YquL_Dx9AkWLT0_i7uC9kGK1wp" 6 0.5 "./build/wallet-query"
+fift -s scripts/wallet.fif "build/main-wallet" "0f9jBy61HhmsQ5qK_vN5EoZwsS2NKyR8prf9pzHcd1N-gDmM" 28 0.5 "./build/wallet-query"
 screen -S lite-client -p 0 -X stuff "sendfile ./build/wallet-query.boc
 "
 sleep 5
--- scripts/05_update_planet.sh
+++ scripts/05_update_planet.sh
@@ -1,5 +1,5 @@
 func -P -o build/updated-planet-code.fif contracts/stdlib.fc contracts/updated-planet-code.fc
-fift -s scripts/update-planet-smc.fif "build/new-planet" 1
-fift -s scripts/wallet.fif "build/main-wallet" "kf_mdBk0Tm0T_zUS4u-WR2YquL_Dx9AkWLT0_i7uC9kGKwHs" 8 0 -B "./build/updated-planet-query.boc" "./build/wallet-query"
+fift -s scripts/update-planet-smc.fif "build/new-planet" 0
+fift -s scripts/wallet.fif "build/main-wallet" "kf9jBy61HhmsQ5qK_vN5EoZwsS2NKyR8prf9pzHcd1N-gGRJ" 29 0.3 -B "./build/updated-planet-query.boc" "./build/wallet-query"
 screen -S lite-client -p 0 -X stuff "sendfile ./build/wallet-query.boc
 "
