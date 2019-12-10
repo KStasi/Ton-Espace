f8668657913f216c6c55b16f4b91912e2d84f1b6
kstasi <nastasiakondaurova@gmail.com>
2019-12-10 21:07:33 +0200

    query id add to msg body

--- scripts/09_transfer_weapon.sh
+++ scripts/09_transfer_weapon.sh
@@ -1,4 +1,4 @@
 fift -s scripts/transfer-weapon-smc.fif "build/new-spaceship"
-fift -s scripts/spaceship.fif "build/new-spaceship" "kf8nchLqQ3kvQlrz_KgCVvUuSj8y7dEUt7UqjZJ_HjBwO1d0" 5 0.3 -B "./build/transfer-weapon-query.boc" "./build/spaceship-query"
+fift -s scripts/spaceship.fif "build/new-spaceship" "kf8nchLqQ3kvQlrz_KgCVvUuSj8y7dEUt7UqjZJ_HjBwO1d0" 7 0.3 -B "./build/transfer-weapon-query.boc" "./build/spaceship-query"
 screen -S lite-client -p 0 -X stuff "sendfile ./build/spaceship-query.boc
 "
