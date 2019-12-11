df90addfbefcad2c56c13964ed9b24c54d26bca7
kstasi <nastasiakondaurova@gmail.com>
2019-12-11 04:53:28 +0200

    store offer scripts added

--- scripts/02_broadcast_new_spaceship.sh
+++ scripts/02_broadcast_new_spaceship.sh
@@ -1,5 +1,5 @@
 #!/bin/sh
-fift -s scripts/wallet.fif "build/main-wallet" "0f-1nFTi-9Caxa1BDQFA_-G5y2D-KhhAqpCU-5NCzyFItPUy" 1 1 "./build/wallet-query"
+fift -s scripts/wallet.fif "build/main-wallet" "0f-1nFTi-9Caxa1BDQFA_-G5y2D-KhhAqpCU-5NCzyFItPUy" 49 3 "./build/wallet-query"
 screen -S lite-client -p 0 -X stuff "sendfile ./build/wallet-query.boc
 "
 sleep 2
--- scripts/12_broadcast_new_exchange.sh
+++ scripts/12_broadcast_new_exchange.sh
@@ -1,5 +1,5 @@
 #!/bin/sh
-fift -s scripts/spaceship.fif "build/new-spaceship" "0f9ogxP0CWs82dGBdHRPQMvV_ERXJZU1msV5ou-WJ05vQj8y" 8 0.3 "./build/spaceship-query"
+fift -s scripts/spaceship.fif "build/new-spaceship" "0f_Dcg3Hu-68tdoPLhhgxETJjbOA27UP8eX7wUdKQfuCy6rt" 12 0.2 "./build/spaceship-query"
 screen -S lite-client -p 0 -X stuff "sendfile ./build/spaceship-query.boc
 "
 sleep 5
