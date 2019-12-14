65e9901af947ebd344bdb9e362b2763f4c394071
kstasi <nastasiakondaurova@gmail.com>
2019-12-14 05:45:19 +0200

    exchange script updated

--- scripts/14_buy_exchange.sh
+++ scripts/14_buy_exchange.sh
@@ -1,4 +1,4 @@
 fift -s scripts/buy-exchange-smc.fif "build/new-weapon"
-fift -s scripts/spaceship.fif "build/new-spaceship" "kf_Dcg3Hu-68tdoPLhhgxETJjbOA27UP8eX7wUdKQfuCy_co" 14 0.2 -B "./build/buy-exchange-query.boc" "./build/spaceship-query"
+fift -s scripts/spaceship.fif "build/new-spaceship" "kf9_uLP6Zaj4IARA8-BpvZf7UAPm0Xc72OBn0ruSBQSFmSPs" 27 0.2 -B "./build/buy-exchange-query.boc" "./build/spaceship-query"
 screen -S lite-client -p 0 -X stuff "sendfile ./build/spaceship-query.boc
 "
